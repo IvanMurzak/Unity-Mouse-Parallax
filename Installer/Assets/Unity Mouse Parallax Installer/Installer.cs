8e694f3436ed2de6ce4abe270643b800a2ebf6e1
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2026-05-24 00:52:12 -0700

    chore: bump version to 1.0.5
    First signed release on the migrated package-template pipeline.

--- Installer/Assets/Unity Mouse Parallax Installer/Installer.cs
+++ Installer/Assets/Unity Mouse Parallax Installer/Installer.cs
@@ -16,7 +16,7 @@ namespace extensions.unity.mouse.parallax.Installer
     public static partial class Installer
     {
         public const string PackageId = "extensions.unity.mouse.parallax";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.5";
 
         static Installer()
         {
